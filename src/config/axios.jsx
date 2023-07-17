e8252ac593c44c802958b3473616d158f6b46385
Pablo <pabloandr@live.cl>
2023-07-16 22:47:12 -0400

    axios

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 const backendUrlLocal = "http://localhost:3000"
 const backendUrlProd = "https://api-5-04ph.onrender.com"
 const axiosClient = axios.create( {
-  baseURL: backendUrlLocal
+  baseURL: backendUrlProd
 } )
 
 export default axiosClient
